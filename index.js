cb63911189b8dbc8de5005eb00418012f2411e7c
Itzgamer5454 <164816315+Itzgamer5454@users.noreply.github.com>
2024-05-17 21:24:20 -0400

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Playing: Made by Itzgamer5454","Used for: Laza's Hangout"];
 
 
 let currentIndex = 0;
